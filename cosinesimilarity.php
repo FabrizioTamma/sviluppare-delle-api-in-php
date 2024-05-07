e1f5675b22c86bb3366f2f3bbfc511a35b467b66
FabrizioTamma <Tamma.fabrizio@panettipitagora.edu.it>
2024-05-07 11:38:20 +0200

    Update cosinesimilarity.php

--- cosinesimilarity.php
+++ cosinesimilarity.php
@@ -9,12 +9,30 @@ function recommend_movis($user_id) {
 
 
 		function cosine_similarity($a, $b){
-			$modulo_a = sqrt(pow $a + pow $b)
-			$modulo_b =
-			$modulo_c =
+			$modulo_a = 0;
+			$modulo_b = 0;
+			$denominatore = 0;
 
-			$similarity =
+            for($i = 0; $i < count ($a); $i++){
+                $modulo_a = $modulo_a + pow($a[$i], 2);
+                $modulo_b = $modulo_b + pow($b[$i], 2);
+            }
 
-			return $similarity()
+            $modulo_a = sqrt($modulo_a);
+            $modulo_b = sqrt($modulo_b);
+
+            $denominatore = $modulo_a * $modulo_b;
+
+            $numeratore = 0;
+
+            for($i = 0; $i < count ($a); $i++){
+                $numeratore = $numeratore + $a[$i] * $b[$i];
+            }
+
+            return $numeratore / $denominatore;
+
+			
+
+			return $similarity();
 		}
 ?>
